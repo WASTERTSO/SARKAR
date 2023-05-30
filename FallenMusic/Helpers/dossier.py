0ec8ef8e4b8da3b7dc6f7fd41bc1fbde35ccf5ce
SARKAROP123 <111958063+SARKAROP123@users.noreply.github.com>
2023-05-30 13:05:03 +0530

    Update dossier.py

--- FallenMusic/Helpers/dossier.py
+++ FallenMusic/Helpers/dossier.py
@@ -23,7 +23,7 @@
 from FallenMusic import BOT_NAME
 
 PM_START_TEXT = """
-𝐁𝐀𝐁𝐘 {0}, 🥀
+𝐁𝐀𝐁𝐘🍸 {0}, 🍹
 ๏ 𝐓𝐇𝐈𝐒** {1} !
 
 🥀 𝐇𝐞𝐥𝐥𝐨, 𝐈 𝐀𝐦 𝐀𝐧 📀 𝐀𝐝𝐯𝐚𝐧𝐜𝐞𝐝 𝐀𝐧𝐝
@@ -31,11 +31,11 @@ PM_START_TEXT = """
 𝐐𝐮𝐚𝐥𝐢𝐭𝐲 𝐀𝐮𝐝𝐢𝐨 𝐀𝐧𝐝 𝐕𝐢𝐝𝐞𝐨 🌷
 
 ┏━━━━━━━━━━━━━━━━━┓
-┣★𝐂𝐑𝐄𝐀𝐓𝐎𝐑 [𝐑𝐀𝐃𝐇𝐀](https://t.me/Cutiiiiiiiiiiie)
-┣★𝐔𝐏𝐃𝐀𝐓𝐄 [𝗝𝗢𝗜𝗡](https://t.me/TKS_JOIN)
-┣★𝐆𝐑𝐎𝐔𝐏 [𝗖𝗛𝗔𝗧 𝗚𝗥𝗢𝗨𝗣](https://t.me/+I67bz3RT2cA3ODU1)
+┣★𝐂𝐑𝐄𝐀𝐓𝐎𝐑🌱 [𝐑𝐀𝐃𝐇𝐀](https://t.me/ll_SARKAR_OP_ll)
+┣★𝐔𝐏𝐃𝐀𝐓𝐄🌱 [𝗝𝗢𝗜𝗡](https://t.me/TKS_JOIN)
+┣★𝐆𝐑𝐎𝐔𝐏🌱 [𝗖𝗛𝗔𝗧 𝗚𝗥𝗢𝗨𝗣](https://t.me/+I67bz3RT2cA3ODU1)
 ┗━━━━━━━━━━━━━━━━━┛
-𝐏𝐎𝐖𝐄𝐑𝐃 𝐁𝐘[𝐑𝐀𝐃𝐇𝐀](https://t.me/Cutiiiiiiiiiiie)
+🌱𝐏𝐎𝐖𝐄𝐑𝐃 𝐁𝐘🥀 [🍷𝐒𝐀𝐑𝐊𝐀𝐑❤️](https://t.me/ll_SARKAR_OP_ll)
 """
 
 START_TEXT = """
